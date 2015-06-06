8e8f23a477344bdae6fc1a6866247fdd20775d9b
Christopher Pilcher <nzchrisp@gmail.com>
2015-06-06 21:45:27 +1200

    Adding shape view factory (abstract factory)

--- DesignPatternsInSwift/GameViewController.swift
+++ DesignPatternsInSwift/GameViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+  private var shapeViewFactory: ShapeViewFactory!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    shapeViewFactory = SquareShapeViewFactory(size: gameView.sizeAvailableForShapes())
+    
     beginNextTurn()
   }
 
@@ -25,13 +30,8 @@ class GameViewController: UIViewController {
     let shape2 = SquareShape()
     shape2.sideLength = Utils.randomBetweenLower(0.3, andUpper: 0.8)
 
-    let availSize = gameView.sizeAvailableForShapes()
-    let shapeView1: ShapeView = SquareShapeView(frame: CGRect(x: 0, y: 0, width: availSize.width * shape1.sideLength, height: availSize.height * shape1.sideLength))
-    shapeView1.shape = shape1
-    let shapeView2: ShapeView = SquareShapeView(frame: CGRect(x: 0, y: 0, width: availSize.width * shape2.sideLength, height: availSize.height * shape2.sideLength))
-    shapeView2.shape = shape2
-    let shapeViews = (shapeView1, shapeView2)
-
+    let shapeViews = shapeViewFactory.makeShapeViewsForShapes((shape1, shape2))
+    
     shapeViews.0.tapHandler = {
       tappedView in
       self.gameView.score += shape1.sideLength >= shape2.sideLength ? 1 : -1
---
+++ DesignPatternsInSwift/ShapeViewFactory.swift
@@ -0,0 +1,36 @@
+//
+//  ShapeViewFactory.swift
+//  DesignPatternsInSwift
+//
+//  Created by Christopher Pilcher on 6/06/15.
+//  Copyright (c) 2015 RepublicOfApps, LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol ShapeViewFactory {
+  var size: CGSize { get set }
+  
+  func makeShapeViewsForShapes(shapes: (Shape, Shape)) -> (ShapeView, ShapeView)
+}
+
+class SquareShapeViewFactory : ShapeViewFactory {
+  var size: CGSize
+  
+  init(size: CGSize) {
+    self.size = size
+  }
+  
+  func makeShapeViewsForShapes(shapes: (Shape, Shape)) -> (ShapeView, ShapeView) {
+    let squareShape1 = shapes.0 as! SquareShape
+    let shapeView1 = SquareShapeView(frame: CGRect(x: 0, y: 0, width: squareShape1.sideLength * size.width, height: squareShape1.sideLength * size.height))
+    shapeView1.shape = squareShape1
+    
+    let squareShape2 = shapes.1 as! SquareShape
+    let shapeView2 = SquareShapeView(frame: CGRect(x: 0, y: 0, width: squareShape2.sideLength * size.width, height: squareShape2.sideLength * size.height))
+    shapeView2.shape = squareShape2
+    
+    return (shapeView1, shapeView2)
+  }
+}
